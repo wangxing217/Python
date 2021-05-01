cfd0fd15168ac6b2de04e3f748cb2d902dfb14bc
王星星 <wangxing@wangxiningdeAir.lan>
2021-05-01 11:05:55 +0800

    python - test

---
+++ main/main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-   
+# project_name: IntelliJ IDEA
+# author: wangxing
+# date: 2021/5/1
+
+import tensorflow as tf
+import numpy as np
+
+print(tf.__version__)
+print(np.arange(3, 7, 1))
