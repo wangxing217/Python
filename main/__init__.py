295b2b7d2c88fbcbde4fb6e4fd74e6224df01dac
王星星 <wangxing@wangxiningdeAir.lan>
2021-05-02 14:38:24 +0800

    python github test

---
+++ main/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-   
+# project_name: IntelliJ IDEA
+# author: wangxing
+# date: 2021/5/2
---
+++ main/main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-   
+# project_name: IntelliJ IDEA
+# author: wangxing
+# date: 2021/5/2
+import tensorflow as tf
+print(tf.__version__)
