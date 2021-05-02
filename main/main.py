07ae5998f891ff149d398dad1959d696ab3a9cdb
王星星 <wangxing@wangxingxingdeMacBook-Air.local>
2021-05-02 21:55:34 +0800

    main.opy 修改

--- main/main.py
+++ main/main.py
@@ -3,7 +3,9 @@
 # author: wangxing
 # date: 2021/5/2
 import tensorflow as tf
+import tensorflow.keras as keras
 print(tf.__version__)
-a = 3
-b = 4
-print(a+b)
+
+if __name__ == '__main__':
+    keras.layers.Dense()
+    print("test")
